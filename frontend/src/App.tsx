9f37570c1b07f48973b58b4a54330fb26f033cc3
Anderson0512 <joao_anderson26@hotmail.com>
2021-05-06 02:00:52 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar/>
+    <div className="container">
       <h1>HELLO WORLD</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer px-4 py-5 bg-dark text-center">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/joao-anderson-esteves/" target="_blank" rel="noreferrer"> Anderson Esteves</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="hhttps://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    )
+}
